c15416d5248362c5ee548edaad88f105b2d17d19
Abhijit Roy <161963891+abhijitxroy@users.noreply.github.com>
2024-08-07 12:35:12 +0530

    Revert "release for 2024.2.1 (#10)"
    This reverts commit e3f784dee5d5ed8d5d52f9f07263d50ac35163f5.

--- diagnostics.py
+++ diagnostics.py
@@ -32,7 +32,7 @@ from modules.printing import print_metadata, print_summary, print_epilog
 from modules.printing.printer import print_ex, enable_stdout_printing
 
 
-VERSION = "2024.2.1"
+VERSION = "2024.2.0"
 API_VERSION = "0.2"
 
 
